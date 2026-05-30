25c0dde855a58a0319e175bac47e92deccb168db
julianvlz21 <julianvluna11@gmail.com>
2026-05-30 14:54:54 -0500

    task 3 y task 4 completas

--- app.js
+++ app.js
@@ -5,16 +5,61 @@ function agregarProducto() {
     let precio = document.getElementById("priceProduct");
     let decription = document.getElementById("decriptionProduct");
     
+    // Validación de datos
     if (!nombre.value || !precio.value || !decription.value){
         alert ("Dato vacio, ingresa los valores de nuevo");
-    } else {
-        alert (`producto ${nombre.value} agregado con exito`);
-    }
+        return;
+    } 
     
-    // nombre.value = "";
-    // precio.value = "";
-    // decription.value = "";
+    alert (`producto ${nombre.value} agregado con exito`);
 
+    // Creación de li para agregarlo al ul del DOM vacio
+    const list = document.getElementById("list");
+    const item = document.createElement("li");
+
+    // Metodo para agregar un elemento a un nodo padre del HTML
+    list.appendChild(item);
+    // item.classList.add("list-item");
+
+    // Creo un objeto para guardar los datos ingresados por el usuario
+    const datosProducto = {
+            nombre: nombre.value,
+            precio: precio.value,
+            decription: decription.value
+        }
     
+    // Agregamos la información del objeto al contenido del item
+    item.textContent = `producto: ${datosProducto.nombre} - precio: $${datosProducto.precio} - ${datosProducto.decription}`;
+    
+    // // Botón eliminar para cada item
+    // const btnEliminar = document.createElement("button");
+    // item.appendChild(btnEliminar);
+    // btnEliminar.textContent = "Eliminar";
+    
+    // btnEliminar.addEventListener("click", () => {
+    //     let confirmacion = confirm(`Seguro que desea eliminar el producto ${nombre.value}`);
+    //     if (!confirmacion){
+    //         list.appendChild(item);
+    //     }else {
+    //         list.removeChild(item);
+    //     }
+    // });
+
+    // // localStorage y visualización de datos por consola
+    // localStorage.setItem("productos", JSON.stringify(datosProducto));
+    // const mostrarNotas = JSON.parse(localStorage.getItem("productos"));
+    // console.log(mostrarNotas);
+
+    // Limpiar campos
+    nombre.value = "";
+    precio.value = "";
+    decription.value = "";
+
+    return datosProducto;
 }
-btnEnviar.addEventListener("click", agregarProducto);
+
+btnEnviar.addEventListener("click", () => {
+    const itemAgregado = agregarProducto();
+    console.log(itemAgregado);
+    
+});
